ffa327a209a766232c059d8a25ff72cce7e94eba
Mariano <merodriguez81@gmail.com>
2017-05-20 04:25:03 -0300

    Mejora en Cadena de Conexion, para que utilice App.config, y metodos  modernos de lectura de ese archivo.

--- Data/GeneralData.cs
+++ Data/GeneralData.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using Model;
+using System.Configuration;
 
 namespace Data
 {
-    public class GeneralData
+    public static class GeneralData
     {
+        public static string CadenaConexion => ConfigurationManager.AppSettings["CS"];
         //public const string CadenaConexion = "Data Source=PHANTOM;Initial Catalog=Kiosco;Integrated Security=True";
-        public const string CadenaConexion = "Data Source=NOTEBOOK;Initial Catalog=Kiosco;Integrated Security=True";
+        //public const string CadenaConexion = "Data Source=NOTEBOOK;Initial Catalog=Kiosco;Integrated Security=True";
 
     }
 }
