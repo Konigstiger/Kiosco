a8a7707f3649575ee8c74c7d8f68a3e40d5852f4
Mariano <merodriguez81@gmail.com>
2017-06-05 02:29:47 -0300

    Refactor DAO para Provedor y Rubro.

--- Data/ProveedorData.cs
+++ Data/ProveedorData.cs
@@ -11,31 +11,24 @@ namespace Data
         public static List<ProveedorView> GetAll()
         {
             var list = new List<ProveedorView>();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Proveedor_GetAll", conn) { CommandType = CommandType.StoredProcedure };
-
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    var p = new ProveedorView {
-                        IdProveedor = (int)rdr["IdProveedor"],
-                        RazonSocial = (string)rdr["RazonSocial"],
-                        Direccion = rdr["Direccion"] != DBNull.Value ? (string)rdr["Direccion"] : "",
-                        Telefono = rdr["Telefono"] != DBNull.Value ? (string)rdr["Telefono"] : "",
-                        Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : ""
-                    };
-
-                    list.Add(p);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Proveedor_GetAll", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            var p = new ProveedorView {
+                                IdProveedor = (int)rdr["IdProveedor"],
+                                RazonSocial = (string)rdr["RazonSocial"],
+                                Direccion = rdr["Direccion"] != DBNull.Value ? (string)rdr["Direccion"] : "",
+                                Telefono = rdr["Telefono"] != DBNull.Value ? (string)rdr["Telefono"] : "",
+                                Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : ""
+                            };
+                            list.Add(p);
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return list;
         }
 
@@ -43,113 +36,95 @@ namespace Data
         public static Proveedor GetByPrimaryKey(int id)
         {
             var c = new Proveedor();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Proveedor_GetByPrimaryKey", conn) { CommandType = CommandType.StoredProcedure };
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Proveedor_GetByPrimaryKey", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var p1 = new SqlParameter("IdProveedor", SqlDbType.BigInt) { Value = id };
-            cmd.Parameters.Add(p1);
+                    var p1 = new SqlParameter("IdProveedor", SqlDbType.BigInt) { Value = id };
+                    cmd.Parameters.Add(p1);
 
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
+                    conn.Open();
+                    var rdr = cmd.ExecuteReader();
 
-                while (rdr.Read()) {
-                    c.IdProveedor = (int)rdr["IdProveedor"];
-                    c.RazonSocial = (string)rdr["RazonSocial"];
-                    c.Direccion = rdr["Direccion"] != DBNull.Value ? (string)rdr["Direccion"] : "";
-                    c.Telefono = rdr["Telefono"] != DBNull.Value ? (string)rdr["Telefono"] : "";
-                    c.PersonaContacto = rdr["PersonaContacto"] != DBNull.Value ? (string)rdr["PersonaContacto"] : "";
-                    c.HorarioAtencion = rdr["HorarioAtencion"] != DBNull.Value ? (string)rdr["HorarioAtencion"] : "";
-                    c.DiasDeVisita = rdr["DiasDeVisita"] != DBNull.Value ? (string)rdr["DiasDeVisita"] : "";
-                    c.Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : "";
-                    c.IdRubro = rdr["IdRubro"] != DBNull.Value ? (int)rdr["IdRubro"] : 0;
-
+                    while (rdr.Read()) {
+                        c.IdProveedor = (int)rdr["IdProveedor"];
+                        c.RazonSocial = (string)rdr["RazonSocial"];
+                        c.Direccion = rdr["Direccion"] != DBNull.Value ? (string)rdr["Direccion"] : "";
+                        c.Telefono = rdr["Telefono"] != DBNull.Value ? (string)rdr["Telefono"] : "";
+                        c.PersonaContacto = rdr["PersonaContacto"] != DBNull.Value ? (string)rdr["PersonaContacto"] : "";
+                        c.HorarioAtencion = rdr["HorarioAtencion"] != DBNull.Value ? (string)rdr["HorarioAtencion"] : "";
+                        c.DiasDeVisita = rdr["DiasDeVisita"] != DBNull.Value ? (string)rdr["DiasDeVisita"] : "";
+                        c.Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : "";
+                        c.IdRubro = rdr["IdRubro"] != DBNull.Value ? (int)rdr["IdRubro"] : 0;
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return c;
         }
 
 
         public static List<ProveedorView> GetAll_GetByRazonSocial(string razonSocial)
         {
-            //TODO: ESTO ES TERRIBLEMENTE REDUNDANTE, ES PARA UNA PRUEBA
             var list = new List<ProveedorView>();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Proveedor_GetByRazonSocial", conn) {
-                CommandType = CommandType.StoredProcedure
-            };
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                var cmd = new SqlCommand("Proveedor_GetByRazonSocial", conn) {
+                    CommandType = CommandType.StoredProcedure
+                };
 
-            var p1 = new SqlParameter("RazonSocial", SqlDbType.VarChar) { Value = razonSocial };
+                var p1 = new SqlParameter("RazonSocial", SqlDbType.VarChar) { Value = razonSocial };
 
-            cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p1);
 
-            try {
                 conn.Open();
-                rdr = cmd.ExecuteReader();
+                using (var rdr = cmd.ExecuteReader()) {
+                    while (rdr.Read()) {
+                        var p = new ProveedorView {
+                            IdProveedor = (int)rdr["IdProveedor"],
+                            RazonSocial = (string)rdr["RazonSocial"],
+                            Direccion = rdr["Direccion"] != DBNull.Value ? (string)rdr["Direccion"] : "",
+                            Telefono = rdr["Telefono"] != DBNull.Value ? (string)rdr["Telefono"] : "",
+                            Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : ""
+                        };
 
-                while (rdr.Read()) {
-                    var p = new ProveedorView {
-                        IdProveedor = (int)rdr["IdProveedor"],
-                        RazonSocial = (string)rdr["RazonSocial"],
-                        Direccion = rdr["Direccion"] != DBNull.Value ? (string)rdr["Direccion"] : "",
-                        Telefono = rdr["Telefono"] != DBNull.Value ? (string)rdr["Telefono"] : "",
-                        Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : ""
-                    };
-
-                    list.Add(p);
+                        list.Add(p);
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return list;
-
         }
+
 
 
         public static int Insert(Proveedor model)
         {
-            int idProveedor;
+            int idProveedor = 0;
 
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Proveedor_Insert", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var cmd = new SqlCommand("Proveedor_Insert", conn) { CommandType = CommandType.StoredProcedure };
+                    var p1 = new SqlParameter("RazonSocial", SqlDbType.VarChar) { Value = model.RazonSocial };
+                    var p2 = new SqlParameter("Direccion", SqlDbType.VarChar) { Value = model.Direccion };
+                    var p3 = new SqlParameter("Telefono", SqlDbType.VarChar) { Value = model.Telefono };
+                    var p4 = new SqlParameter("PersonaContacto", SqlDbType.VarChar) { Value = model.PersonaContacto };
+                    var p5 = new SqlParameter("HorarioAtencion", SqlDbType.VarChar) { Value = model.HorarioAtencion };
+                    var p6 = new SqlParameter("DiasDeVisita", SqlDbType.VarChar) { Value = model.DiasDeVisita };
 
-            var p1 = new SqlParameter("RazonSocial", SqlDbType.VarChar) { Value = model.RazonSocial };
-            var p2 = new SqlParameter("Direccion", SqlDbType.VarChar) { Value = model.Direccion };
-            var p3 = new SqlParameter("Telefono", SqlDbType.VarChar) { Value = model.Telefono };
-            var p4 = new SqlParameter("PersonaContacto", SqlDbType.VarChar) { Value = model.PersonaContacto };
-            var p5 = new SqlParameter("HorarioAtencion", SqlDbType.VarChar) { Value = model.HorarioAtencion };
-            var p6 = new SqlParameter("DiasDeVisita", SqlDbType.VarChar) { Value = model.DiasDeVisita };
+                    var p7 = new SqlParameter("Notas", SqlDbType.VarChar) { Value = model.Notas };
+                    var p8 = new SqlParameter("IdRubro", SqlDbType.Int) { Value = model.IdRubro };
 
-            var p7 = new SqlParameter("Notas", SqlDbType.VarChar) { Value = model.Notas };
-            var p8 = new SqlParameter("IdRubro", SqlDbType.Int) { Value = model.IdRubro };
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
+                    cmd.Parameters.Add(p3);
+                    cmd.Parameters.Add(p4);
+                    cmd.Parameters.Add(p5);
+                    cmd.Parameters.Add(p6);
+                    cmd.Parameters.Add(p7);
+                    cmd.Parameters.Add(p8);
 
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
-            cmd.Parameters.Add(p3);
-            cmd.Parameters.Add(p4);
-            cmd.Parameters.Add(p5);
-            cmd.Parameters.Add(p6);
-            cmd.Parameters.Add(p7);
-            cmd.Parameters.Add(p8);
-
-            try {
-                conn.Open();
-                idProveedor = (int)cmd.ExecuteScalar();
-
-            }
-            finally {
-                conn.Close();
+                    conn.Open();
+                    idProveedor = (int)cmd.ExecuteScalar();
+                }
             }
 
             return idProveedor;
--- Data/RubroData.cs
+++ Data/RubroData.cs
@@ -11,129 +11,96 @@ namespace Data
         public static List<RubroView> GetAll()
         {
             var list = new List<RubroView>();
-
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-
-            var cmd = new SqlCommand("Rubro_GetAll", conn) { CommandType = CommandType.StoredProcedure };
-
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    var p = new RubroView {
-                        IdRubro = (int)rdr["IdRubro"],
-                        Descripcion = (string)rdr["Descripcion"]
-                    };
-
-                    // Obtener los resultados de cada columna
-
-                    list.Add(p);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Rubro_GetAll", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            var p = new RubroView {
+                                IdRubro = (int)rdr["IdRubro"],
+                                Descripcion = (string)rdr["Descripcion"]
+                            };
+                            // Obtener los resultados de cada columna
+                            list.Add(p);
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return list;
         }
 
 
-        public static List<RubroView> SearchByParameters(string Nombre)
+        public static List<RubroView> SearchByParameters(string nombre)
         {
             var list = new List<RubroView>();
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Rubro_SearchByParameters", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            // create a connection object
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+                    var p1 = new SqlParameter("nombre", SqlDbType.VarChar) { Value = nombre };
+                    var p2 = new SqlParameter("IdProvincia", SqlDbType.Int) { Value = null };
 
-            // declare the SqlDataReader, which is used in both the try block and the finally block
-            //Contiene los datos
-            SqlDataReader rdr = null;
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
 
-            //contiene los detalles de que tabla / sp se leerá.
-            var cmd = new SqlCommand("Rubro_SearchByParameters", conn) { CommandType = CommandType.StoredProcedure };
-
-            var p1 = new SqlParameter("Nombre", SqlDbType.VarChar) { Value = Nombre };
-            var p2 = new SqlParameter("IdProvincia", SqlDbType.Int) { Value = null };
-
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
-
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    var p = new RubroView {
-                        IdRubro = (int)rdr["IdRubro"],
-                        Descripcion = (string)rdr["Descripcion"]
-                    };
-
-                    list.Add(p);
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            var p = new RubroView {
+                                IdRubro = (int)rdr["IdRubro"],
+                                Descripcion = (string)rdr["Descripcion"]
+                            };
+                            list.Add(p);
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return list;
         }
+
 
         public static int Insert(Rubro c)
         {
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Rubro_Insert", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            //contiene los detalles de que tabla / sp se ejecutará.
-            var cmd = new SqlCommand("Rubro_Insert", conn) { CommandType = CommandType.StoredProcedure };
+                    var p0 = new SqlParameter("IdRubro", SqlDbType.Int) { Direction = ParameterDirection.Output };
+                    var p1 = new SqlParameter("Descripcion", SqlDbType.VarChar) { Value = c.Descripcion };
 
-            var p0 = new SqlParameter("IdRubro", SqlDbType.Int) { Direction = ParameterDirection.Output };
-            var p1 = new SqlParameter("Descripcion", SqlDbType.VarChar) { Value = c.Descripcion };
+                    cmd.Parameters.Add(p0);
+                    cmd.Parameters.Add(p1);
 
-            cmd.Parameters.Add(p0);
-            cmd.Parameters.Add(p1);
-
-            try {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                c.IdRubro = (int)p0.Value;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    c.IdRubro = (int)p0.Value;
+                }
             }
-            catch {
-            }
-            finally {
-                conn.Close();
-            }
-
             return c.IdRubro;
         }
+
 
         public static Rubro GetByPrimaryKey(int idRubro)
         {
             var c = new Rubro();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Rubro_GetByPrimaryKey", conn) {CommandType = CommandType.StoredProcedure};
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Rubro_GetByPrimaryKey", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var p1 = new SqlParameter("IdRubro", SqlDbType.Int) {Value = idRubro};
-            cmd.Parameters.Add(p1);
+                    var p1 = new SqlParameter("IdRubro", SqlDbType.Int) { Value = idRubro };
+                    cmd.Parameters.Add(p1);
 
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    c.IdRubro = (int)rdr["IdRubro"];
-                    c.Descripcion = (string)rdr["Descripcion"];
-                    c.Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : "";
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            c.IdRubro = (int)rdr["IdRubro"];
+                            c.Descripcion = (string)rdr["Descripcion"];
+                            c.Notas = rdr["Notas"] != DBNull.Value ? (string)rdr["Notas"] : "";
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return c;
         }
     }
