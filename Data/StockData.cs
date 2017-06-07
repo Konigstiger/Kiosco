660af5dfa5fd6b561e506234e285fe88c5e53359
Mariano <merodriguez81@gmail.com>
2017-06-07 18:46:32 -0300

    Pequeño refactor DAO.

--- Data/StockData.cs
+++ Data/StockData.cs
@@ -11,30 +11,24 @@ namespace Data
         public static List<StockView> GetAll()
         {
             var list = new List<StockView>();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Stock_GetAll", conn) { CommandType = CommandType.StoredProcedure };
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Stock_GetAll", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    var p = new StockView {
-                        IdStock = (long)rdr["IdStock"],
-                        Cantidad = (int)rdr["Cantidad"],
-                        IdDeposito = (int)rdr["IdDeposito"],
-                        IdProducto = (long)rdr["IdProducto"]
-                    };
-
-                    list.Add(p);
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            var p = new StockView {
+                                IdStock = (long)rdr["IdStock"],
+                                Cantidad = (int)rdr["Cantidad"],
+                                IdDeposito = (int)rdr["IdDeposito"],
+                                IdProducto = (long)rdr["IdProducto"]
+                            };
+                            list.Add(p);
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return list;
         }
 
@@ -42,60 +36,47 @@ namespace Data
         public static StockView GetByPrimaryKey(long idStock)
         {
             var c = new StockView();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Stock_GetByPrimaryKey", conn) { CommandType = CommandType.StoredProcedure };
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                var cmd = new SqlCommand("Stock_GetByPrimaryKey", conn) { CommandType = CommandType.StoredProcedure };
 
-            var p1 = new SqlParameter("IdStock", SqlDbType.Int) { Value = idStock };
-            cmd.Parameters.Add(p1);
+                var p1 = new SqlParameter("IdStock", SqlDbType.Int) { Value = idStock };
+                cmd.Parameters.Add(p1);
 
-            try {
                 conn.Open();
-                rdr = cmd.ExecuteReader();
+                using (var rdr = cmd.ExecuteReader()) {
 
-                while (rdr.Read()) {
-                    c.IdStock = (long)rdr["IdStock"];
-                    c.Cantidad = (int)rdr["Cantidad"];
-                    c.IdDeposito = (int)rdr["IdDeposito"];
-                    c.IdProducto = (long)rdr["IdProducto"];
+                    while (rdr.Read()) {
+                        c.IdStock = (long)rdr["IdStock"];
+                        c.Cantidad = (int)rdr["Cantidad"];
+                        c.IdDeposito = (int)rdr["IdDeposito"];
+                        c.IdProducto = (long)rdr["IdProducto"];
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return c;
         }
-
 
 
         public static long Insert(Stock model)
         {
             long idStock;
-
             var c = new StockView();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Stock_Insert", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var cmd = new SqlCommand("Stock_Insert", conn) { CommandType = CommandType.StoredProcedure };
+                    var p1 = new SqlParameter("Cantidad", SqlDbType.Int) { Value = model.Cantidad };
+                    var p2 = new SqlParameter("IdDeposito", SqlDbType.Int) { Value = model.IdDeposito };
+                    var p3 = new SqlParameter("IdProducto", SqlDbType.BigInt) { Value = model.IdProducto };
 
-            var p1 = new SqlParameter("Cantidad", SqlDbType.Int) { Value = model.Cantidad };
-            var p2 = new SqlParameter("IdDeposito", SqlDbType.Int) { Value = model.IdDeposito};
-            var p3 = new SqlParameter("IdProducto", SqlDbType.BigInt) { Value = model.IdProducto };
-            
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
-            cmd.Parameters.Add(p3);
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
+                    cmd.Parameters.Add(p3);
 
-            try {
-                conn.Open();
-                idStock = (long)cmd.ExecuteScalar();
-
+                    conn.Open();
+                    idStock = (long)cmd.ExecuteScalar();
+                }
             }
-            finally {
-                conn.Close();
-            }
-
             return idStock;
         }
 
@@ -103,28 +84,22 @@ namespace Data
         public static long Update(Stock model)
         {
             long idStock;
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Stock_Update", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+                    var p1 = new SqlParameter("IdDeposito", SqlDbType.Int) { Value = model.IdDeposito };
+                    var p2 = new SqlParameter("IdProducto", SqlDbType.BigInt) { Value = model.IdProducto };
+                    var p3 = new SqlParameter("Cantidad", SqlDbType.Int) { Value = model.Cantidad };
 
-            var cmd = new SqlCommand("Stock_Update", conn) { CommandType = CommandType.StoredProcedure };
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
+                    cmd.Parameters.Add(p3);
 
-            var p1 = new SqlParameter("IdDeposito", SqlDbType.Int) { Value = model.IdDeposito };
-            var p2 = new SqlParameter("IdProducto", SqlDbType.BigInt) { Value = model.IdProducto };
-            var p3 = new SqlParameter("Cantidad", SqlDbType.Int) { Value = model.Cantidad };
-
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
-            cmd.Parameters.Add(p3);
-
-            try {
-                conn.Open();
-                idStock = (long)cmd.ExecuteScalar();
-
+                    conn.Open();
+                    idStock = (long)cmd.ExecuteScalar();
+                }
             }
-            finally {
-                conn.Close();
-            }
-
             return idStock;
         }
 
@@ -132,55 +107,45 @@ namespace Data
         public static Stock GetByParameters(Stock producto)
         {
             var c = new Stock();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Stock_GetByParameters", conn) { CommandType = CommandType.StoredProcedure };
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Stock_GetByParameters", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var p1 = new SqlParameter("IdProducto", SqlDbType.BigInt) { Value = producto.IdProducto };
-            var p2 = new SqlParameter("IdDeposito", SqlDbType.Int) { Value = producto.IdDeposito };
+                    var p1 = new SqlParameter("IdProducto", SqlDbType.BigInt) { Value = producto.IdProducto };
+                    var p2 = new SqlParameter("IdDeposito", SqlDbType.Int) { Value = producto.IdDeposito };
 
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
 
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    c.IdStock = (long)rdr["IdStock"];
-                    c.Cantidad = (int)rdr["Cantidad"];
-                    c.IdProducto = (long)rdr["IdProducto"];
-                    c.IdDeposito = (int)rdr["IdDeposito"];
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            c.IdStock = (long)rdr["IdStock"];
+                            c.Cantidad = (int)rdr["Cantidad"];
+                            c.IdProducto = (long)rdr["IdProducto"];
+                            c.IdDeposito = (int)rdr["IdDeposito"];
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return c;
         }
 
+
         public static bool Delete(Stock model)
         {
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Stock_Delete", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var cmd = new SqlCommand("Stock_Delete", conn) { CommandType = CommandType.StoredProcedure };
+                    var p0 = new SqlParameter("IdProducto", SqlDbType.VarChar) { Value = model.IdProducto };
 
-            var p0 = new SqlParameter("IdProducto", SqlDbType.VarChar) { Value = model.IdProducto };
+                    cmd.Parameters.Add(p0);
 
-            cmd.Parameters.Add(p0);
-
-            try {
-                conn.Open();
-                model.IdProducto = (long)cmd.ExecuteScalar();
-                return true;
-            }
-            catch {
-                return false;
-            }
-            finally {
-                conn.Close();
+                    conn.Open();
+                    model.IdProducto = (long)cmd.ExecuteScalar();
+                    return true;
+                }
             }
         }
     }
--- Data/UnidadData.cs
+++ Data/UnidadData.cs
@@ -11,66 +11,49 @@ namespace Data
         public static List<UnidadView> GetAll()
         {
             var list = new List<UnidadView>();
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Unidad_GetAll", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-
-            var cmd = new SqlCommand("Unidad_GetAll", conn) { CommandType = CommandType.StoredProcedure };
-
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    var p = new UnidadView {
-                        IdUnidad = (int)rdr["IdUnidad"],
-                        Descripcion = (string)rdr["Descripcion"],
-                        Simbolo = (string)rdr["Simbolo"],
-                        Unidades = rdr["Unidades"] != DBNull.Value ? (int)rdr["Unidades"] : 1
-                    };
-
-                    // Obtener los resultados de cada columna
-
-                    list.Add(p);
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            var p = new UnidadView {
+                                IdUnidad = (int)rdr["IdUnidad"],
+                                Descripcion = (string)rdr["Descripcion"],
+                                Simbolo = (string)rdr["Simbolo"],
+                                Unidades = rdr["Unidades"] != DBNull.Value ? (int)rdr["Unidades"] : 1
+                            };
+                            list.Add(p);
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return list;
         }
 
 
         public static int Insert(Unidad c)
         {
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Unidad_Insert", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            //contiene los detalles de que tabla / sp se ejecutará.
-            var cmd = new SqlCommand("Unidad_Insert", conn) { CommandType = CommandType.StoredProcedure };
+                    var p0 = new SqlParameter("IdUnidad", SqlDbType.Int) { Direction = ParameterDirection.Output };
+                    var p1 = new SqlParameter("Descripcion", SqlDbType.VarChar) { Value = c.Descripcion };
+                    var p2 = new SqlParameter("Simbolo", SqlDbType.VarChar) { Value = c.Simbolo };
+                    var p3 = new SqlParameter("Unidades", SqlDbType.Int) { Value = c.Unidades };
 
-            var p0 = new SqlParameter("IdUnidad", SqlDbType.Int) { Direction = ParameterDirection.Output };
-            var p1 = new SqlParameter("Descripcion", SqlDbType.VarChar) { Value = c.Descripcion };
-            var p2 = new SqlParameter("Simbolo", SqlDbType.VarChar) { Value = c.Simbolo };
-            var p3 = new SqlParameter("Unidades", SqlDbType.Int) { Value = c.Unidades };
+                    cmd.Parameters.Add(p0);
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
+                    cmd.Parameters.Add(p3);
 
-            cmd.Parameters.Add(p0);
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
-            cmd.Parameters.Add(p3);
-
-            try {
-                conn.Open();
-                cmd.ExecuteNonQuery();
-                c.IdUnidad = (int)p0.Value;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    c.IdUnidad = (int)p0.Value;
+                }
             }
-            catch {
-            }
-            finally {
-                conn.Close();
-            }
-
             return c.IdUnidad;
         }
 
@@ -78,58 +61,52 @@ namespace Data
         public static Unidad GetByPrimaryKey(int idUnidad)
         {
             var c = new Unidad();
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
-            SqlDataReader rdr = null;
-            var cmd = new SqlCommand("Unidad_GetByPrimaryKey", conn) {CommandType = CommandType.StoredProcedure};
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Unidad_GetByPrimaryKey", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var p1 = new SqlParameter("IdUnidad", SqlDbType.Int) {Value = idUnidad};
-            cmd.Parameters.Add(p1);
+                    var p1 = new SqlParameter("IdUnidad", SqlDbType.Int) { Value = idUnidad };
+                    cmd.Parameters.Add(p1);
 
-            try {
-                conn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read()) {
-                    c.IdUnidad = (int)rdr["IdUnidad"];
-                    c.Descripcion = (string)rdr["Descripcion"];
-                    c.Simbolo = rdr["Simbolo"] != DBNull.Value ? (string)rdr["Simbolo"] : "";
-                    c.Unidades = rdr["Unidades"] != DBNull.Value ? (int)rdr["Unidades"] : 1;
+                    conn.Open();
+                    using (var rdr = cmd.ExecuteReader()) {
+                        while (rdr.Read()) {
+                            c.IdUnidad = (int)rdr["IdUnidad"];
+                            c.Descripcion = (string)rdr["Descripcion"];
+                            c.Simbolo = rdr["Simbolo"] != DBNull.Value ? (string)rdr["Simbolo"] : "";
+                            c.Unidades = rdr["Unidades"] != DBNull.Value ? (int)rdr["Unidades"] : 1;
+                        }
+                    }
                 }
             }
-            finally {
-                rdr?.Close();
-                conn.Close();
-            }
-
             return c;
         }
 
+
         public static int Update(Unidad model)
         {
-            var conn = new SqlConnection(GeneralData.CadenaConexion);
+            using (var conn = new SqlConnection(GeneralData.CadenaConexion)) {
+                using (var cmd = new SqlCommand("Unidad_Update", conn)) {
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-            var cmd = new SqlCommand("Unidad_Update", conn) { CommandType = CommandType.StoredProcedure };
+                    var p0 = new SqlParameter("IdUnidad", SqlDbType.Int) { Value = model.IdUnidad };
+                    var p1 = new SqlParameter("Descripcion", SqlDbType.VarChar) { Value = model.Descripcion };
+                    var p2 = new SqlParameter("Simbolo", SqlDbType.VarChar) { Value = model.Simbolo };
+                    var p3 = new SqlParameter("Unidades", SqlDbType.VarChar) { Value = model.Unidades };
 
-            var p0 = new SqlParameter("IdUnidad", SqlDbType.Int) { Value = model.IdUnidad};
-            var p1 = new SqlParameter("Descripcion", SqlDbType.VarChar) { Value = model.Descripcion };
-            var p2 = new SqlParameter("Simbolo", SqlDbType.VarChar) { Value = model.Simbolo };
-            var p3 = new SqlParameter("Unidades", SqlDbType.VarChar) { Value = model.Unidades };
+                    cmd.Parameters.Add(p0);
+                    cmd.Parameters.Add(p1);
+                    cmd.Parameters.Add(p2);
+                    cmd.Parameters.Add(p3);
 
-            cmd.Parameters.Add(p0);
-            cmd.Parameters.Add(p1);
-            cmd.Parameters.Add(p2);
-            cmd.Parameters.Add(p3);
-
-            try {
-                conn.Open();
-                model.IdUnidad = (int)cmd.ExecuteScalar();
-
+                    conn.Open();
+                    model.IdUnidad = (int)cmd.ExecuteScalar();
+                }
             }
-            finally {
-                conn.Close();
-            }
-
             return model.IdUnidad;
         }
+
+
     }
+
 }
