5c8b760bf5e3d3f9abfa0ed5d4a1a497c700d4b9
Mariano Rodriguez <mariano.rodriguez@cedi.com.ar>
2018-04-26 10:56:25 -0300

    Prueba base para armar patron Repository.

---
+++ Modelos/Interfaces/IProductoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Interfaces
+{
+    public interface IProductoRepository
+    {
+        IEnumerable<Producto> GetAll();
+        Producto Get(long id);
+        Producto Add(Producto item);
+        void Remove(long id);
+        bool Update(Producto item);
+
+    }
+}
