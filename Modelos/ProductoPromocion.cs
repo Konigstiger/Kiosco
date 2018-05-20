8b7cd0faa0cddb88e0ae7657185eb4d86c12747c
Mariano <merodriguez81@gmail.com>
2018-05-20 02:01:26 -0300

    Agregado de clases de Promociones

---
+++ Modelos/ProductoPromocion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Model
+{
+    public class ProductoPromocion: IEntidad
+    {
+        /*
+         	[IdProductoPromocion] [int] NOT NULL,
+	[IdPromocion] [int] NOT NULL,
+	[IdProducto] [bigint] NOT NULL,
+	[Cantidad] [int] NOT NULL,
+	[PrecioPromocion] [decimal](8, 2) NULL,
+	[PorcentajeDescuento] [decimal](8, 2) NULL,
+	[Notas] [varchar](100) NULL,
+             */
+
+        public long IdProductoPromocion { get; set; }
+        public int IdPromocion { get; set; }
+        public long IdProducto { get; set; }
+        public decimal PrecioPromocion { get; set; }
+        public decimal PorcentajeDescuento { get; set; }
+
+        public string Notas { get; set; }
+
+        public bool Validate()
+        {
+            return true;
+        }
+    }
+}
---
+++ Modelos/Promocion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Model.View;
+
+namespace Model
+{
+    public class Promocion : IEntidad
+    {
+
+        /*
+	[IdPromocion] [int] NOT NULL,
+	[Descripcion] [varchar](50) NOT NULL,
+	[Importe] [decimal](8, 2) NOT NULL,
+	[FechaInicio] [date] NOT NULL,
+	[FechaFin] [date] NOT NULL,
+	[Notas] [varchar](255) NULL,
+        */
+
+        public Promocion()
+        {
+        }
+
+        public int IdPromocion { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Importe { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
+        public string Notas { get; set; }
+
+
+        public bool Validate()
+        {
+            //TODO: Este metodo, tiene como objetivo realizar validaciones basicas.
+            //por ejemplo, ver si su codigo es -1. De ser asi, retornar false.
+            //se pueden agregar muchas mas validaciones. Tambien podrian ir en el Controlador. 
+            //Pero creo que me gustan aqui.
+
+            //if (IdPromocion.Equals(-1)) return false;
+
+            if (FechaFin < FechaInicio)
+                return false;
+            //TODO: Pensar y agregar mas posibles validaciones de cosas que podrian estar mal en el modelo.           
+
+            return true;
+        }
+
+        /*
+        //TODO: Esto es algo nuevo que hago, puede ser muy util!
+        public static explicit operator Promocion(PromocionView v)
+        {
+            return new Promocion() {
+                IdPromocion = v.IdPromocion,
+                CodigoBarras = v.CodigoBarras,
+                Descripcion = v.Descripcion,
+                IdMarca = null,
+                IdRubro = null,
+                IdUnidad = null,
+                PrecioCostoPromedio = 1,
+                PrecioVenta = v.Precio,
+                Capacidad = v.Capacidad,
+                SoloAdultos = null,
+                StockMaximo = 0,
+                StockMinimo = 0
+            };
+            
+        }
+        */
+    }
+}
